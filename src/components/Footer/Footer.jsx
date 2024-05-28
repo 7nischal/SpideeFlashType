bd8f74826268bf9796641d3f58ffcfd1ecb20711
7nischal <112294161+7nischal@users.noreply.github.com>
2024-05-28 17:55:27 -0400

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer-container">
-            <a href="https://github.com/7nischal"
+            <a href="https://github.com/7nischal/SpideeFlashType"
                 className="footer-link"
                 target="_blank"
                 rel="noreferrer">
